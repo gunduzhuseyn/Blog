1a0a82ee6435810ac64f1f4e983052224f7aaeef
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2019-02-01 02:22:58 +0300

    added and integrated theme

--- src/blog/myblog/urls.py
+++ src/blog/myblog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='myblog/home.html'), name='home_url'),
+    path('', TemplateView.as_view(template_name='myblog/index.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='myblog/about.html'), name='about_me'),
+    path('post', TemplateView.as_view(template_name='myblog/post.html'), name='post'),
+    path('contact/', TemplateView.as_view(template_name='myblog/contact.html'), name='contact'),
 ]
