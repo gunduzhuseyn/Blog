ba344313b27c834ca836c40fb21355868d1fa1db
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2019-02-22 04:36:07 +0300

    header image upload for posts implemented

--- src/blog/myblog/models.py
+++ src/blog/myblog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
+
 import datetime
+import os
+
+
+def post_header_image_dir(instance, filename):
+    return '{0}/header_image.jpg'.format(instance.id)
 
 
 class Post(models.Model):
@@ -10,9 +17,19 @@ class Post(models.Model):
     date_created = models.DateField(default=datetime.date.today)
     date_published = models.DateField(default=datetime.date.today)
     is_published = models.BooleanField(default=False)
+    header_image = models.ImageField(upload_to=post_header_image_dir, null=True)
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if getattr(self, 'header_image_changed', True):
+            old_header_image = Post.objects.get(id=self.id).header_image
+            if old_header_image:
+                path = old_header_image.path
+                print(path)
+                os.remove(path)
+        super(Post, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'pk': self.id})
