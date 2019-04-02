c95eb549bb159abb59ae2f36f51b39d46489bf31
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2019-04-02 17:47:37 +0300

    update db settings for prod

--- src/blog/blog/settings/prod.py
+++ src/blog/blog/settings/prod.py
@@ -2,6 +2,17 @@ from .base import *
 
 ALLOWED_HOSTS = []
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DJANGO_DB_NAME'],
+        'USER': os.environ['DJANGO_DB_USER'],
+        'PASSWORD': os.environ['DJANGO_DB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
 if os.environ['HTTPS_ENABLED']:
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
