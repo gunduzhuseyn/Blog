f35f2724a201bc7668d7587a54f73c090d8639d8
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2019-04-07 13:41:01 +0300

    Change Debug mode

--- src/blog/blog/settings/base.py
+++ src/blog/blog/settings/base.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DJANGO_DEBUG']
 
 # Application definition
 
--- src/blog/blog/settings/prod.py
+++ src/blog/blog/settings/prod.py
@@ -1,5 +1,7 @@
 from .base import *
 
+DEBUG = False
+
 ALLOWED_HOSTS = ['www.gunduzhuseyn.com', 'gunduzhuseyn.com']
 
 DATABASES = {
