0854aa9e40ffca8a676c5a6131ace10fa3042d95
Gunduz Huseyn Lee <gunduzhuseynli12@gmail.com>
2019-04-13 13:27:34 +0300

    urls.py bug fix

--- src/blog/myblog/urls.py
+++ src/blog/myblog/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     re_path('^category/(?P<slug>[-\w]+)/$', HomeView.as_view(), name='home_with_category_url'),
     path('contact/', ContactFormView.as_view(), name='contact_url'),
     path('contact/success/', ContactSuccessView.as_view(), name='contact_success_url'),
-    path('400.html/', TemplateView(template_name='400.html'), name='400_error_page'),
-    path('403.html/', TemplateView(template_name='403.html'), name='403_error_page'),
-    path('404.html/', TemplateView(template_name='404.html'), name='404_error_page'),
-    path('500.html/', TemplateView(template_name='500.html'), name='500_error_page'),
+    path('400.html/', TemplateView.as_view(template_name='400.html'), name='400_error_page'),
+    path('403.html/', TemplateView.as_view(template_name='403.html'), name='403_error_page'),
+    path('404.html/', TemplateView.as_view(template_name='404.html'), name='404_error_page'),
+    path('500.html/', TemplateView.as_view(template_name='500.html'), name='500_error_page'),
 ]
